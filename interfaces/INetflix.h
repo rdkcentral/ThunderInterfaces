8d16257b9fa130a9c34c319ffec8754c24bae077
sebaszm <s.szczepaniak@metrological.com>
2021-02-09 12:39:10 +0100

    [INetflix] Remove redundant API covered by IApplication, mark some JSON-RPC API as deprecated

--- interfaces/INetflix.h
+++ interfaces/INetflix.h
@@ -36,7 +36,6 @@ namespace Exchange {
         struct EXTERNAL INotification : virtual public Core::IUnknown {
             enum { ID = ID_NETFLIX_NOTIFICATION };
 
-            // Signal changes on the subscribed namespace..
             virtual void StateChange(const INetflix::state state) = 0;
             virtual void Exit(const uint32_t exitCode) = 0;
         };
@@ -44,12 +43,7 @@ namespace Exchange {
         virtual void Register(INetflix::INotification* netflix) = 0;
         virtual void Unregister(INetflix::INotification* netflix) = 0;
 
-        virtual string GetESN() const = 0;
-
-        virtual void FactoryReset() = 0;
         virtual void SystemCommand(const string& command) = 0;
-        virtual void Language(const string& language) = 0;
-        virtual void SetVisible(bool visibility) = 0;
     };
 }
 }
