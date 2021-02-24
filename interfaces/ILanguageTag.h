9551f6c31dc5ce2a97f25311c69395cd7ffded79
Vinod Sathyaseelan <vinod_sathyaseelan@comcast.com>
2021-02-23 19:11:38 -0800

    [METRO-124] Create a new LanguageAdministrator plugin
    - Reason for change :  Enhancement
         Currently languages need to be set per application, which also does not work when the application is not active.
         We should extend the Location sync to provide the current active langue/locale (by default determined by the location) and
         the possibility to override this from the interface (e.g. when the middle ware allows the user to select a different language).
    
         Also a notification of this change/event should be available so JS can respsond to this event.
    
    - Solution :
         Instead of extending LocationSync, we have agreed to introduce a new plugin named LanguageAdministrator
         to take care of the language related requirements.

---
+++ interfaces/ILanguageTag.h
@@ -0,0 +1,56 @@
+/*
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2020 RDK Management
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+#include <utility>
+
+#include "Module.h"
+
+namespace WPEFramework {
+namespace Exchange {
+
+    // @json
+    struct EXTERNAL ILanguageTag : virtual public Core::IUnknown {
+        enum { ID = ID_LANGUAGETAG };
+
+        // @event
+        struct EXTERNAL INotification : virtual public Core::IUnknown {
+            enum { ID = ID_LANGUAGETAG_NOTIFICATION };
+
+            virtual ~INotification() {}
+
+            // @brief Notify that the Language tag has been changed
+            // @param language new LangauageTag value.
+            virtual void LanguageChanged(const std::string& language) = 0;
+
+        };
+
+        virtual ~ILanguageTag() {}
+
+        virtual void Register(ILanguageTag::INotification* sink) = 0;
+        virtual void Unregister(const ILanguageTag::INotification* sink) = 0;
+        // @property
+        // @brief Current application user interface language tag
+        // @param language Language string as per RFC5646 (e.g. en)
+        virtual uint32_t Language(std::string& language /* @out */) const = 0;
+        virtual uint32_t Language(const std::string& language) = 0;
+
+    };
+}
+}
