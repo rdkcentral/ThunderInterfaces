23c405b2b6909ba17f46f910a6352b6ee382242c
madanagopaltcomcast <35588028+madanagopaltcomcast@users.noreply.github.com>
2021-03-24 18:31:07 +0530

    Update IEssosSample.h

--- interfaces/IEssosSample.h
+++ interfaces/IEssosSample.h
@@ -9,12 +9,9 @@ namespace Exchange {
         enum { ID = ID_ESSOS_SAMPLE };
 
         virtual ~IEssosSample() {}
-        virtual uint32_t Initialize(PluginHost::IShell* service) = 0;
-        virtual void Deinitialize(PluginHost::IShell* service) = 0;
-        virtual uint32_t Configure(PluginHost::IShell* service) = 0;
-        virtual uint32_t setWaylandDisplay(const string &input) = 0;
-        virtual uint32_t start() = 0;
-        virtual uint32_t stop() = 0;
+        virtual uint32_t SetWaylandDisplay(const string &input) = 0;
+        virtual uint32_t Start() = 0;
+        virtual uint32_t Stop() = 0;
     };
 
 } // Exchange
