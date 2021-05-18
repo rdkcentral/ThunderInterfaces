3504ca9743176fa912498d3bce45ed73df313a6c
wissem <golliwissem@hotmail.com>
2021-05-18 12:42:21 +0200

    metrol-275: adding DeviceMetada to the Interface :model name, model year, system integrator name, friendly name and platform name

--- interfaces/IDeviceInfo.h
+++ interfaces/IDeviceInfo.h
@@ -71,6 +71,20 @@ namespace Exchange {
         virtual uint32_t HDCP(CopyProtection& supportedHDCP /*@out*/) const = 0;
 
 
+
+    };
+
+    struct EXTERNAL IDeviceMetadata : virtual public Core::IUnknown {
+        enum { ID = ID_DEVICE_METADATA };
+
+        virtual ~IDeviceMetadata() {}
+
+
+        virtual uint32_t ModelName(string& value/*@out*/) const = 0; 
+        virtual uint32_t ModelYear(string& value/*@out*/) const = 0; 
+        virtual uint32_t FriendlyName(string& value/*@out*/) const = 0; 
+        virtual uint32_t SystemIntegratorName(string& value/*@out*/) const = 0; 
+        virtual uint32_t PlatformName(string& value/*@out*/) const = 0; 
     };
 }
 }
