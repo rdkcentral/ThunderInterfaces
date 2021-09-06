2ba0570500f24ca904c5cd25603cd3433b7cd9e6
sramani-metro <71630728+sramani-metro@users.noreply.github.com>
2021-09-06 17:27:58 +0530

    Dev/pattern encryption scheme (#71)
    * Add Support for Encryption Scheme and Pattern to be passed in.
    
    * Add Metrological Copyright header
    
    Co-authored-by: Pierre Wielders <pierre@wielders.net>

--- interfaces/IContentDecryption.h
+++ interfaces/IContentDecryption.h
@@ -53,6 +53,142 @@ namespace Exchange {
 
         virtual uint32_t Register(IContentDecryption::INotification* notification) { return Core::ERROR_NOT_SUPPORTED; };
         virtual uint32_t Unregister(IContentDecryption::INotification* notification) { return Core::ERROR_NOT_SUPPORTED; };
+    
+    };
+    
+    class DataExchange : public Core::SharedBuffer {
+    private:
+        DataExchange() = delete;
+        DataExchange(const DataExchange&) = delete;
+        DataExchange& operator=(const DataExchange&) = delete;rivate:
+        struct Administration {
+            uint32_t Status;
+            uint8_t KeyId[17];
+            uint8_t IVLength;
+            uint8_t IV[24];
+            uint8_t EncScheme;
+            uint32_t PatternEncBlocks;
+            uint32_t PatternClearBlocks;
+            bool InitWithLast15;
+        };
+
+    public:
+        DataExchange(const string& name)
+            : Core::SharedBuffer(name.c_str())
+        {
+        }
+        DataExchange(const string& name, const uint32_t bufferSize)
+            : Core::SharedBuffer(name.c_str(), 
+                Core::File::USER_READ    |
+                Core::File::USER_WRITE   |
+                Core::File::USER_EXECUTE |
+                Core::File::GROUP_READ   |
+                Core::File::GROUP_WRITE  |
+                Core::File::OTHERS_READ  |
+                Core::File::OTHERS_WRITE,
+                bufferSize,
+                sizeof(Administration))
+        {
+            Administration* admin = reinterpret_cast<Administration*>(AdministrationBuffer());
+            // Clear the administration space before using it.
+            ::memset(admin, 0, sizeof(Administration));
+        }
+        ~DataExchange() {}
+
+    public:
+        inline void Status(uint32_t status)
+        {
+            reinterpret_cast<Administration*>(AdministrationBuffer())->Status = status;
+        }
+        inline uint32_t Status() const
+        {
+            return (reinterpret_cast<const Administration*>(AdministrationBuffer())
+                        ->Status);
+        }
+        inline void InitWithLast15(bool initWithLast15)
+        {
+            reinterpret_cast<Administration*>(AdministrationBuffer())->InitWithLast15 = initWithLast15;
+        }
+        inline bool InitWithLast15() const
+        {
+            return (reinterpret_cast<const Administration*>(AdministrationBuffer())
+                        ->InitWithLast15);
+        }
+        void SetIV(const uint8_t ivDataLength, const uint8_t ivData[])
+        {
+            Administration* admin = reinterpret_cast<Administration*>(AdministrationBuffer());
+            ASSERT(ivDataLength <= sizeof(Administration::IV));
+            admin->IVLength = (ivDataLength > sizeof(Administration::IV) ? sizeof(Administration::IV)
+                                                                        : ivDataLength);
+            ::memcpy(admin->IV, ivData, admin->IVLength);
+            if (admin->IVLength < sizeof(Administration::IV)) {
+                ::memset(&(admin->IV[admin->IVLength]), 0,
+                    (sizeof(Administration::IV) - admin->IVLength));
+            }
+        }
+        void SetEncScheme(const uint8_t encScheme)
+        {
+            Administration* admin = reinterpret_cast<Administration*>(AdministrationBuffer());
+            admin->EncScheme = encScheme;
+        }
+
+        const uint8_t EncScheme()
+        {
+            Administration* admin = reinterpret_cast<Administration*>(AdministrationBuffer());
+            return admin->EncScheme;
+        }
+
+        void SetEncPattern(const uint32_t encBlocks, const uint32_t clearBlocks)
+        {
+            Administration* admin = reinterpret_cast<Administration*>(AdministrationBuffer());
+            admin->PatternEncBlocks = encBlocks;
+            admin->PatternClearBlocks = clearBlocks;
+        }
+
+        void EncPattern(uint32_t& encBlocks, uint32_t& clearBlocks)
+        {
+            Administration* admin = reinterpret_cast<Administration*>(AdministrationBuffer());
+            encBlocks = admin->PatternEncBlocks;
+            clearBlocks = admin->PatternClearBlocks;
+        }
+
+        void Write(const uint32_t length, const uint8_t* data)
+        {
+
+            if (Core::SharedBuffer::Size(length) == true) {
+                SetBuffer(0, length, data);
+            }
+        }
+        void Read(const uint32_t length, uint8_t* data) const
+        {
+            GetBuffer(0, length, data);
+        }
+        const uint8_t* IVKey() const
+        {
+            const Administration* admin = reinterpret_cast<const Administration*>(AdministrationBuffer());
+            return (admin->IV);
+        }
+        uint8_t IVKeyLength() const
+        {
+            const Administration* admin = reinterpret_cast<const Administration*>(AdministrationBuffer());
+            return (admin->IVLength);
+        }
+        void KeyId(const uint8_t length, const uint8_t buffer[])
+        {
+            Administration* admin = reinterpret_cast<Administration*>(AdministrationBuffer());
+            ASSERT(length <= 16);
+            admin->KeyId[0] = (length <= 16 ? length : 16);
+            if (length != 0) {
+                ::memcpy(&(admin->KeyId[1]), buffer, admin->KeyId[0]);
+            }
+        }
+        const uint8_t* KeyId(uint8_t& length) const
+        {
+            const Administration* admin = reinterpret_cast<const Administration*>(AdministrationBuffer());
+            length = admin->KeyId[0];
+            ASSERT(length <= 16);
+            return (length > 0 ? &admin->KeyId[1] : nullptr);
+        }
     };
 }
 }
