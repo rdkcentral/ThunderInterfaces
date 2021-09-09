1eeeb0de19b6222e5b2eb3f9a16730826623447c
wissem <golliwissem@hotmail.com>
2021-09-09 15:40:12 +0200

    changed pid type into int.

--- interfaces/ISecureShellServer.h
+++ interfaces/ISecureShellServer.h
@@ -59,7 +59,7 @@ namespace Exchange {
         virtual uint32_t Activate_dropbear(const string& inputConfigs) = 0;
         virtual uint32_t Deactivate_dropbear() = 0;
         virtual size_t GetActiveSessionsCount() = 0 ;
-        virtual uint32_t CloseClientSession(const uint64_t pid) = 0;
+        virtual uint32_t CloseClientSession(const int pid) = 0;
         virtual IClientSession::IIterator* ActiveClientsIter() = 0;
 
     };
