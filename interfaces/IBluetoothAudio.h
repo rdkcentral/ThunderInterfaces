98a52487c2c804927c6af88fa551cbf23e09d670
sebaszm <s.szczepaniak@metrological.com>
2021-10-15 18:18:27 +0200

    [IBluetoothAudio] Replace Start/Stop methods with Speed, rename Open and Close

--- interfaces/IBluetoothAudio.h
+++ interfaces/IBluetoothAudio.h
@@ -40,41 +40,37 @@ namespace Exchange {
 
             struct Format {
                 uint32_t SampleRate     /* @brief Source sample rate in Hz (e.g. 44100)*/ ;
-                uint16_t FrameRate      /* @brief Source frame rate in Hz (e.g. 24) */ ;
+                uint16_t FrameRate      /* @brief Source frame rate in centiseconds (e.g. 2400) */ ;
                 uint8_t Resolution      /* @brief Sampling resolution in bits per sample (e.g. 16) */ ;
                 uint8_t Channels        /* @brief Number of audio channels in the source stream (e.g. 2) */ ;
             };
 
-            // @brief Opens a streaming session and configures the sink device
+            // @brief Acquires the device and configures the streaming session
             // @param connector Name of the shared location the sample data will be written to
             // @brief properties Audio properties of the input stream
             // @retval ERROR_UNAVAILABLE The audio sink is currently being used by another client
-            // @retval ERROR_ILLEGAL_STATE The audio sink is not connected or already open
+            // @retval ERROR_ILLEGAL_STATE The audio sink is not connected or already aqcuired
             // @retval ERROR_NOT_SUPPORTED Stream format is not supported by the sink or codec or otherwise invalid
             // @retval ERROR_OPENING_FAILED Failed to open the shared location
             // @retval ERROR_ASYNC_FAILED Device error, failed to open and/or configure
-            virtual uint32_t Open(const string& connector, const Format& format) = 0;
+            virtual uint32_t Acquire(const string& connector, const Format& format) = 0;
 
-            // @brief Closes the streaming session
-            // @retval ERROR_ILLEGAL_STATE The audio sink is not currently open
+            // @brief Releases the device and closes the streaming session
+            // @retval ERROR_ILLEGAL_STATE The audio sink has not been acquired
             // @retval ERROR_ASYNC_FAILED Device error, failed to close the sink
-            virtual uint32_t Close() = 0;
+            virtual uint32_t Relinquish() = 0;
 
-            // @brief Starts audio stream playback
-            // @retval ERROR_ILLEGAL_STATE The audio sink is not open or already streaming
+            // @brief Sets stream playback speed
+            // @param speed New playback speed in per cent to set (e.g. 100)
+            // @retval ERROR_ILLEGAL_STATE The audio sink has not been acquired
+            // @retval ERROR_NOT_SUPPORTED Unsupported speed value given
             // @retval ERROR_ASYNC_FAILED Device error, failed to start/pause playback
-            virtual uint32_t Start() = 0;
-
-            // @brief Stops audio stream playback
-            // @description Data still in the buffer will be played back to the end.
-            // @retval ERROR_ILLEGAL_STATE The audio sink is not currently streaming
-            // @retval ERROR_ASYNC_FAILED Device error, failed to start/pause playback
-            virtual uint32_t Stop() = 0;
+            virtual uint32_t Speed(const int8_t speed) = 0;
 
             // @brief Current playback time
-            // @param Playback mileage since sink opening (in miliseconds)
-            // @retval ERROR_ILLEGAL_STATE The audio sink is not currently open
-            virtual uint32_t Time(uint32_t& position /* @out */) const = 0;
+            // @param position Sets or gets current playback mileage (in miliseconds), value of -1 is ignored
+            // @retval ERROR_ILLEGAL_STATE The audio sink has not been acquired
+            virtual uint32_t Time(uint32_t& position /* @inout */) const = 0;
         };
 
         enum state : uint8_t {
@@ -146,7 +142,6 @@ namespace Exchange {
         virtual uint32_t Assign(const string& address) = 0;
 
         // @brief Revokes a Bluetooth device from audio playback
-        // @param address Address of the bluetooth device to revoke
         // @retval ERROR_ALREADY_RELEASED No device is currently assigned
         virtual uint32_t Revoke() = 0;
 
@@ -170,17 +165,17 @@ namespace Exchange {
         virtual uint32_t SupportedDRMs(IDRMSchemeIterator*& drms /* @out */) const = 0;
 
         // @property
-        // @brief Currently used codec properties
+        // @brief Properites of the currently used codec
         // @retval ERROR_ILLEGAL_STATE Currently not streaming
         virtual uint32_t Codec(CodecProperties& properties /* @out */) const = 0;
 
         // @property
-        // @brief Currently used DRM scheme properties
+        // @brief Properties of the currently used DRM scheme
         // @retval ERROR_ILLEGAL_STATE Currently not streaming
         virtual uint32_t DRM(DRMProperties& properties /* @out */) const = 0;
 
         // @property
-        // @brief Current output stream properties
+        // @brief Properties of the current output stream
         // @retval ERROR_ILLEGAL_STATE Currently not streaming
         virtual uint32_t Stream(StreamProperties& properties /* @out */) const = 0;
     };
