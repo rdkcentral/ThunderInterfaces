a5b8f5c983477963985a771a4795c705fef87286
Nikita Poltorapavlo <Nikita_Poltorapavlo2@cable.comcast.com>
2022-01-31 13:20:17 +0200

    RDKTV-11877: timezone interface
    Reason for change: ITimeZone interface for
    get/set timezone (ex. in a file).
    Test Procedure: Builds.
    Risks: Low
    Signed-off-by: Nikita Poltorapavlo <npoltorapavlo@productengine.com>

---
+++ interfaces/ITimeZone.h
@@ -0,0 +1,44 @@
+/*
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2020 RDK Management
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "Module.h"
+
+namespace WPEFramework {
+namespace Exchange {
+
+struct EXTERNAL ITimeZone : virtual public Core::IUnknown {
+  enum { ID = ID_TIMEZONE };
+
+  struct EXTERNAL INotification : virtual public Core::IUnknown {
+    enum { ID = ID_TIMEZONE_NOTIFICATION };
+
+    virtual void TimeZoneChanged(const string &timeZone) = 0;
+  };
+
+  virtual void Register(ITimeZone::INotification* sink) = 0;
+  virtual void Unregister(ITimeZone::INotification* sink) = 0;
+
+  virtual string GetTimeZone() = 0;
+  virtual bool SetTimeZone(const string &timeZone) = 0;
+};
+
+} // namespace Exchange
+} // namespace WPEFramework
