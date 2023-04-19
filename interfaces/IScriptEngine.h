7513b2b92281630528e93c13a9fed92ab4e3078e
Pierre Wielders <pierre@wielders.net>
2023-04-19 10:19:32 +0200

    [NEWIF] Add a watchdog and a scriptengine interface.

---
+++ interfaces/IScriptEngine.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "Module.h"
+
+namespace WPEFramework {
+namespace Exchange {
+
+    // @json 
+    struct EXTERNAL IEngine : virtual public Core::IUnknown {
+        enum { ID = ID_SCRIPT_ENGINE };
+
+        struct EXTERNAL INotification : virtual public Core::IUnknown {
+            enum { ID = ID_SCRIPT_ENGINE_NOTIFICATION };
+
+            // Signal changes on the subscribed namespace..
+            virtual void URLChanged(const string& URL) = 0;
+        };
+
+        virtual Core::hresult Register(INotification* sink /* @in */) = 0;
+        virtual Core::hresult Unregister(const INotification* sink /* @in */) = 0;
+
+        // @property
+        // @brief Script to be loaded into the engine and to be executed.
+        // @param url Loaded URL (e.g. https://example.com)
+        virtual Core::hresult URL(string& url /* @out */) const = 0;
+        virtual Core::hresult URL(const string& url) = 0;
+    };
+}
+}
+
