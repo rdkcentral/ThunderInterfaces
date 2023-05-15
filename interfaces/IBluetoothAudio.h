ed1c13296f7251db0b5c1995df3eb2ba7e57ab83
sebaszm <s.szczepaniak@metrological.com>
2023-05-15 10:22:06 +0200

    Add IBluetoothAudioSource interface

--- interfaces/IBluetoothAudio.h
+++ interfaces/IBluetoothAudio.h
@@ -2,7 +2,7 @@
  * If not stated otherwise in this file or this component's LICENSE file the
  * following copyright and licenses apply:
  *
- * Copyright 2021 Metrological
+ * Copyright 2021,2023 Metrological
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -39,43 +39,19 @@ namespace Exchange {
             enum { ID = ID_BLUETOOTHAUDIOSINK_CONTROL };
 
             struct Format {
-                uint32_t SampleRate     /* @brief Source sample rate in Hz (e.g. 44100)*/ ;
-                uint16_t FrameRate      /* @brief Source frame rate in cHz (e.g. 2400) */ ;
-                uint8_t Resolution      /* @brief Sampling resolution in bits per sample (e.g. 16) */ ;
-                uint8_t Channels        /* @brief Number of audio channels in the source stream (e.g. 2) */ ;
+                uint32_t SampleRate;
+                uint16_t FrameRate;
+                uint8_t Resolution;
+                uint8_t Channels;
             };
 
-            // @brief Acquires the device and configures the streaming session
-            // @param connector Name of the shared location the sample data will be written to
-            // @brief properties Audio properties of the input stream
-            // @retval ERROR_UNAVAILABLE The audio sink is currently being used by another client
-            // @retval ERROR_ILLEGAL_STATE The audio sink is not connected or already aqcuired
-            // @retval ERROR_NOT_SUPPORTED Stream format is not supported by the sink or codec or otherwise invalid
-            // @retval ERROR_OPENING_FAILED Failed to open the shared location
-            // @retval ERROR_ASYNC_FAILED Device error, failed to open and/or configure
             virtual uint32_t Acquire(const string& connector, const Format& format) = 0;
-
-            // @brief Releases the device and closes the streaming session
-            // @retval ERROR_ILLEGAL_STATE The audio sink has not been acquired
-            // @retval ERROR_ASYNC_FAILED Device error, failed to close the sink
             virtual uint32_t Relinquish() = 0;
 
-            // @brief Sets stream playback speed
-            // @param speed New playback speed in per cent to set (e.g. 100)
-            // @retval ERROR_ILLEGAL_STATE The audio sink has not been acquired
-            // @retval ERROR_NOT_SUPPORTED Unsupported speed value given
-            // @retval ERROR_ASYNC_FAILED Device error, failed to start/pause playback
             virtual uint32_t Speed(const int8_t speed) = 0;
 
-            // @brief Gets current playback time
-            // @param position Current playback position since acquiring the sink (in miliseconds)
-            // @retval ERROR_ILLEGAL_STATE The audio sink has not been acquired
-            virtual uint32_t Time(uint32_t& position /* @out */) const = 0;
-
-            // @brief Gets current audio delay
-            // @param delay Time for the next audio sample to become audible (in samples)
-            // @retval ERROR_ILLEGAL_STATE The audio sink has not been acquired
-            virtual uint32_t Delay(uint32_t& delay /* @out */) const = 0;
+            virtual uint32_t Time(uint32_t& positionMs /* @out */) const = 0;
+            virtual uint32_t Delay(uint32_t& delayMs /* @out */) const = 0;
         };
 
         enum state : uint8_t {
@@ -136,13 +112,11 @@ namespace Exchange {
             virtual void Updated() = 0;
         };
 
-        // @brief Registers the state update callback
-        // @param Callback to register or nullptr to deregister
-        // @retval ERROR_ILLEGAL_STATE A calback is already registered with the audio sink
         virtual uint32_t Callback(ICallback* callback) = 0;
 
         // @brief Assigns a Bluetooth device for audio playback
         // @param address Address of the bluetooth device to assign
+        // @retval ERROR_BAD_REQUEST Device address value is invalid
         // @retval ERROR_ALREADY_CONNECTED A device is already assigned
         virtual uint32_t Assign(const string& address) = 0;
 
@@ -153,6 +127,7 @@ namespace Exchange {
         // @property
         // @brief Sink audio latency
         // @param latency Audio latency of the sink in milliseconds (e.g. 20)
+        // @retval ERROR_BAD_REQUEST Latency value is invalid
         // @retval ERROR_ILLEGAL_STATE No device is currently assigned
         virtual uint32_t Latency(const int16_t latency) = 0;
         virtual uint32_t Latency(int16_t& latency /* @out */) const = 0;
@@ -178,17 +153,99 @@ namespace Exchange {
 
         // @property
         // @brief Properites of the currently used codec
-        // @retval ERROR_ILLEGAL_STATE Currently not streaming
+        // @retval ERROR_ILLEGAL_STATE Currently not configured or streaming
         virtual uint32_t Codec(CodecProperties& properties /* @out */) const = 0;
 
         // @property
         // @brief Properties of the currently used DRM scheme
-        // @retval ERROR_ILLEGAL_STATE Currently not streaming
+        // @retval ERROR_ILLEGAL_STATE Currently not configured or streaming
         virtual uint32_t DRM(DRMProperties& properties /* @out */) const = 0;
 
         // @property
         // @brief Properties of the current output stream
-        // @retval ERROR_ILLEGAL_STATE Currently not streaming
+        // @retval ERROR_ILLEGAL_STATE Currently not configured or streaming
+        virtual uint32_t Stream(StreamProperties& properties /* @out */) const = 0;
+    };
+
+    // @json
+    struct EXTERNAL IBluetoothAudioSource: virtual public Core::IUnknown {
+
+        enum { ID = ID_BLUETOOTHAUDIOSOURCE };
+
+        struct EXTERNAL IControl : virtual public Core::IUnknown {
+
+            // This interface is not exposed via JSON-RPC
+
+            enum { ID = ID_BLUETOOTHAUDIOSOURCE_CONTROL };
+
+            using Format = IBluetoothAudioSink::IControl::Format;
+
+            struct EXTERNAL ICallback : virtual public Core::IUnknown {
+
+                enum { ID = ID_BLUETOOTHAUDIOSOURCE_CONTROL_CALLBACK };
+
+                virtual uint32_t Acquired(const string& connector, const Format& format) = 0;
+                virtual void Relinquished() = 0;
+
+                virtual void SpeedChanged(const uint8_t speed) = 0;
+            };
+
+            virtual uint32_t Callback(ICallback* callback) = 0;
+
+            virtual uint32_t Relinquish() = 0;
+
+            virtual uint32_t Time(uint32_t& positionMs /* @out */) const = 0;
+        };
+
+        enum state : uint8_t {
+            DISCONNECTED,           // No audio source device connected
+            CONNECTED,              // Audio source connected, but not yet configured
+            READY,                  // Source ready and configured for streaming
+            STREAMING               // Audio is currently being streamed
+        };
+
+        enum devicetype : uint8_t {
+            UNKNOWN,
+            PLAYER,
+            MICROPHONE,
+            TUNER,
+            MIXER
+        };
+
+        using CodecProperties = IBluetoothAudioSink::CodecProperties;
+        using DRMProperties = IBluetoothAudioSink::DRMProperties;
+        using StreamProperties = IBluetoothAudioSink::StreamProperties;
+
+        // @event
+        struct INotification : virtual public Core::IUnknown {
+
+            enum { ID = ID_BLUETOOTHAUDIOSOURCE_NOTIFICATION };
+
+            // @brief Signals audio source state change or stream properties update
+            virtual void Updated() = 0;
+        };
+
+        virtual uint32_t Register(INotification* notification) = 0;
+        virtual uint32_t Unregister(const INotification* notification) = 0;
+
+        // @property
+        // @brief Bluetooth address of the source device
+        // @retval ERROR_ILLEGAL_STATE Source device not connected
+        virtual uint32_t Device(string& address /* @out */) const = 0;
+
+        // @property
+        // @brief Properites of the currently used codec
+        // @retval ERROR_ILLEGAL_STATE Currently not ready
+        virtual uint32_t Codec(CodecProperties& properties /* @out */) const = 0;
+
+        // @property
+        // @brief Properties of the currently used DRM scheme
+        // @retval ERROR_ILLEGAL_STATE Currently not ready
+        virtual uint32_t DRM(DRMProperties& properties /* @out */) const = 0;
+
+        // @property
+        // @brief Properties of the current output stream
+        // @retval ERROR_ILLEGAL_STATE Currently not ready
         virtual uint32_t Stream(StreamProperties& properties /* @out */) const = 0;
     };
 
