aef19f174f6fd648e5edd5884d401576292acd5f
VeithMetro <m.daniluk@metrological.com>
2023-07-27 14:38:13 +0200

    Using the enum from Core::Messaging::Metadata in interfaces

--- interfaces/IMessageControl.h
+++ interfaces/IMessageControl.h
@@ -21,6 +21,7 @@
 
 #include "Module.h"
 
+// @insert <core/MessageStore.h>
 // @insert <com/IIteratorType.h>
 
 namespace WPEFramework {
@@ -32,13 +33,7 @@ struct EXTERNAL IMessageControl : virtual public Core::IUnknown {
 
     enum { ID = ID_MESSAGE_CONTROL };
 
-    enum messagetype : uint8_t {
-        TRACING        = 1,
-        LOGGING        = 2,
-        REPORTING      = 3,
-        STANDARD_OUT   = 4,
-        STANDARD_ERROR = 5
-    };
+    using messagetype = Core::Messaging::Metadata::type;
 
     struct Control {
         messagetype type /* @brief Type of message */;
