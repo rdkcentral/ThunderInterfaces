226ef5ac3f5ac1e80c350cccb329c403f9897953
szymon-sobczak-comcast <Szymon_Sobczak@comcast.com>
2023-10-07 02:58:57 -0700

    add ISsoWeather iterface identificator definitions

--- interfaces/Ids.h
+++ interfaces/Ids.h
@@ -354,7 +354,10 @@ namespace Exchange {
         ID_SCRIPT_ENGINE_NOTIFICATION                = ID_SCRIPT_ENGINE + 1,
 
         ID_TEXT_TO_SPEECH                            = RPC::IDS::ID_EXTERNAL_INTERFACE_OFFSET + 0x4C0,
-        ID_TEXT_TO_SPEECH_NOTIFICATION               = ID_TEXT_TO_SPEECH + 1
+        ID_TEXT_TO_SPEECH_NOTIFICATION               = ID_TEXT_TO_SPEECH + 1,
+
+        ID_SSOWEATHER                                = RPC::IDS::ID_EXTERNAL_INTERFACE_OFFSET + 0x4D0,
+        ID_SSOWEATHER_NOTIFICATION                   = ID_SSOWEATHER + 1
     };
 }
 }
