5e3cbab5a3233542c1a0345c485813a00a281d36
Pierre Wielders <pierre@wielders.net>
2024-01-11 18:43:15 +0100

    [IZIGWAVE] Update the interface to use the new generators.. (#318)

--- interfaces/IZigWave.h
+++ interfaces/IZigWave.h
@@ -30,6 +30,31 @@ namespace WPEFramework {
         struct EXTERNAL IZigWave : virtual public Core::IUnknown {
             enum { ID = ID_ZIGWAVE };
 
+            struct Device {
+                enum type : uint8_t {
+                    END_DEVICE,
+                    ROUTER,
+                    CONTROLLER
+                };
+
+                uint32_t Parent /* @brief address of the Parent */;
+                uint32_t Address /* @brief address of the device */;
+                type Type /* @brief type of the device */;
+                uint16_t Manufacturer  /* @brief manufactured of this device */;
+                string Metadata /* metadata associated with this device */;
+            };
+
+            struct Controller {
+                bool Permutable;
+                string Port;
+                uint32_t Id;
+                uint8_t Major;
+                uint8_t Minor;
+                uint16_t Nodes;
+            };
+
+            using IDeviceIterator = RPC::IIteratorType<Device, ID_ZIGWAVE_DEVICE_ITERATOR>;
+
             // @property
             // @brief To allow new devices to the network, the controller should be placed
             //        into an accepting mode. By enabling this mode, the controller can 
@@ -37,29 +62,26 @@ namespace WPEFramework {
             // @param enabled (true) or disable (false) the accepting mode.
             // 
             /* @alt accept */
-            virtual uint32_t Permutable(const uint32_t address /* @index */, bool& enabled /* @out */) const = 0;
-            virtual uint32_t Permutable(const uint32_t address /* @index */, const bool enabled) = 0;
+            virtual Core::hresult Permutable(const uint32_t address /* @index */, bool& enabled /* @out */) const = 0;
+            virtual Core::hresult Permutable(const uint32_t address /* @index */, const bool enabled) = 0;
 
             // @property
-            // @brief To allow new devices to the network, the controller should be placed
-            //        into an accepting mode. By enabling this mode, the controller can 
-            //        accept new devices.
-            // @param enabled (true) or disable (false) the accepting mode.
+            // @brief Get the information of available devices. An id of 0xFFFFFFFF means 
+            // all devices available by this plugin.
             // 
-            virtual uint32_t Device(const uint32_t id /* @index */, string& metadata /* @out */) const = 0;
-
-            // @brief All devices currently available
-            // @return devices: array of ids from the available devices.
-            /* @json:omit */
-            virtual uint32_t Devices(RPC::IValueIterator*& devices /* @out */) const = 0;
+            virtual Core::hresult Devices(const uint32_t id /* @index */, IDeviceIterator*& devices /* @out */) const = 0;
 
             // @brief Bind the *out* from the soure to the *in* of the destination
             // @return: The success of the binding action.
-            virtual uint32_t Bind(const uint32_t source, const uint32_t destination) = 0;
+            virtual Core::hresult Bind(const uint32_t source, const uint32_t destination) = 0;
 
             // @brief Unbind the *out* from the soure to the *in* of the destination
             // @return: The success of the unbinding action.
-            virtual uint32_t Unbind(const uint32_t source, const uint32_t destination) = 0;
+            virtual Core::hresult Unbind(const uint32_t source, const uint32_t destination) = 0;
+
+            // @brief return the Metadata associated with the controller for this instance.
+            // @return: The success of the Metdata action.
+            virtual Core::hresult Metadata(Controller& metadata /* @out */) const = 0;
         };
     }
 }
