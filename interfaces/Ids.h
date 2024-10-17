946ff661e5e1fc8b1a1d261d9e47115fcc2d6c2f
Boopathi Vanavarayan <boopathi_vanavarayan@comcast.com>
2024-10-17 06:25:19 +0000

    Removed changes from Ids.h file, since its creating hunk error

--- interfaces/Ids.h
+++ interfaces/Ids.h
@@ -362,9 +362,8 @@ namespace Exchange {
         ID_TEXT_TO_SPEECH_NOTIFICATION               = ID_TEXT_TO_SPEECH + 1,
 
         ID_USER_SETTINGS                             = RPC::IDS::ID_EXTERNAL_INTERFACE_OFFSET + 0x4D0,
-        ID_USER_SETTINGS_NOTIFICATION                = ID_USER_SETTINGS + 1,
+        ID_USER_SETTINGS_NOTIFICATION                = ID_USER_SETTINGS + 1
 	
-	ID_MIGRATION_PREPARER                        = RPC::IDS::ID_EXTERNAL_INTERFACE_OFFSET + 0x4E0
     };
 }
 }
