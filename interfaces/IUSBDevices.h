b7fde9ca4f90a0966994bcec5f6b8a09f225046e
Pierre Wielders <pierre@wielders.net>
2024-11-16 08:10:51 +0100

    [CREATE] USB interface for generc USB device handling..

---
+++ interfaces/IUSBDevices.h
@@ -0,0 +1,123 @@
+#pragma once
+ 
+#include "Module.h"
+
+// @stubgen:include <com/IIteratorType.h>
+ 
+namespace Thunder {
+
+namespace Exchange {
+
+// @json
+struct EXTERNAL IUSBDevices : virtual public Core::IUnknown {
+
+  enum { ID = ID_USB_DEVICE };
+
+  ~IUSBDevices() override = default;
+
+  enum Speed : uint8_t {
+      SPEED_LOW = 1,
+      SPEED_FULL = 2,
+      SPEED_HIGH = 3,
+      SPEED_SUPER = 4
+  };
+
+  enum Flags : uint8_t {
+      FLAGS_AVAILABLE = 1,
+      FLAGS_SWITCHABLE = 2
+  };
+
+  enum Status : uint8_t {
+      STATUS_ACTIVE = 0x01,
+      STATUS_NO_DEVICE_CONNECTED = 0x02,
+      STATUS_SELF_POWERED = 0x03
+  };
+
+  struct ProductMetadata {
+      uint8_t languageId    /* @brief language id present on the device */;
+      string  serialNumber  /* @brief unicode string representing the serial number of the device */;
+      string  manufacturer  /* @brief unicode string representing the manufacturer of the device */;
+      string  product       /* @brief unicode string representing the product */;
+  };
+
+  struct DeviceMetadata { 
+    string          deviceName     /* @brief Name of the USB device */;
+    uint32_t        parentId       /* @brief Parent Node ID of the device */;
+    Status          status         /* @brief current device status */;
+    uint8_t         deviceLevel    /* @brief device level */;
+    uint8_t         portNumber     /* @brief port number of USB on which the device is attached */;
+    uint8_t         protocol       /* @brief Protocol supported by the device */;
+    Speed           busSpeed       /* @brief Speed of the device */;
+    string          serialNumber   /* @brief Serial number of the device */;
+    Flags           flags          /* @brief Flags of the device */;
+    uint16_t        vendorId       /* @brief the vendor Id assigned to this device */;
+    uint16_t        productId      /* @brief the product Id assigned to this device */;
+    uint8_t         deviceClass    /* @brief USB class of the device as per USB specificiation */ ;
+    uint8_t         deviceSubclass /* @brief USB sub class of the device as per USB specificiation */;
+    string          devicePath     /* @brief the path to be used for the USB device ex: /dev/sdX */;
+    uint32_t        features       /* @brief Features supported by the device - reserved */;
+    uint8_t         numLanguageIds /* @brief number of language ids present on the device */;
+    ProductMetadata productInfo1;
+    ProductMetadata productInfo2;
+    ProductMetadata productInfo3;
+    ProductMetadata productInfo4;
+  };
+
+  // @event 
+  struct EXTERNAL INotification : virtual public Core::IUnknown {
+    enum { ID = ID_USB_DEVICE_NOTIFICATION };
+
+    ~INotification() override = default;
+
+    // @brief Device Plugged in on the bus notification
+    // @param USBDevice: USB information of the plugged in device
+    virtual void Announced(const DeviceMetadata& device) = 0;
+
+    // @brief Device removed from the bus notification
+    // @param USBDevice: USB information of the plugged in device
+    virtual void Revoked(const DeviceMetadata& device) = 0;
+  };
+
+  struct EXTERNAL IChannel : virtual public Core::IUnknown {
+
+      enum { ID = ID_USB_DEVICE };
+
+      ~IChannel() override = default;
+  };
+
+  /** Register notification interface */
+  virtual Core::hresult Register(INotification *notification) = 0;
+
+  /** Unregister notification interface */
+  virtual Core::hresult Unregister(const INotification *notification) = 0;
+  
+  /** Gets s list of USB devices hooked up to this device.*/
+  // @brief Get a string array defning al devices hooked up to this device.
+  // @param iterator : List of USB devices (deviceName).
+  virtual Core::hresult Devices(RPC::IStringIterator*& iterator /* @out */) const = 0;
+
+  /** Gets s list of USB devices hooked up to this device, filtered on vendorId/ProductId.*/
+  // @brief Get a string array defning al devices hooked up to this device cmpliant to the given vendorId/ProductId.
+  // @param iterator : List of USB devices (deviceName).
+  virtual Core::hresult Devices(const uint16_t vendorId, const uint16_t productId, RPC::IStringIterator*& iterator /* @out */) const = 0;
+
+  /** Gets the device metadata of a connected USB Devices.*/
+  // @brief Get the metadata information about the name passed in the paramater.
+  // @param device : all the metadata of the requested deviceName device.
+  virtual Core::hresult Device(const string& deviceName, struct DeviceMetadata& device /* @out */ ) const = 0;
+
+  /** Binds the respective driver for the device */
+  // @brief Bind the respective driver for the device */
+  // @param deviceName: Name of the device
+  virtual Core::hresult Bind(const string& deviceName /* @in */, IChannel*& channel /* @out */ ) const = 0;
+
+  /** Unbinds the respective driver for the device */
+  // @brief Unbind the respective driver for the device */
+  // @param deviceName: Name of the device
+  virtual Core::hresult Unbind(const IChannel*& channel /* @out */) const = 0;
+};
+ 
+} // namespace Exchange
+
+} // namespace Thunder
+
