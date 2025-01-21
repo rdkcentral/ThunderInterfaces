63ecc3b6cf61bb04e0c6d5b4f74f7fcc8fb5048e
VeithMetro <m.daniluk@metrological.com>
2025-01-21 12:32:40 +0100

    Create ILocationSync and remove LocationSync.json

--- interfaces/Ids.h
+++ interfaces/Ids.h
@@ -385,6 +385,9 @@ namespace Exchange {
         ID_WATERMARK                                 = RPC::IDS::ID_EXTERNAL_INTERFACE_OFFSET + 0x510,
         ID_WATERMARK_NOTIFICATION                    = ID_WATERMARK + 2,
 
+        ID_LOCATIONSYNC                              = RPC::IDS::ID_EXTERNAL_INTERFACE_OFFSET + 0x520,
+        ID_LOCATIONSYNC_NOTIFICATION                 = ID_LOCATIONSYNC + 1,
+
     };
 }
 }
