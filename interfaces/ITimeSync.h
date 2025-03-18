a3a8fdef4756fe9dfd7db8ce04200f93625eddaa
VeithMetro <m.daniluk@metrological.com>
2025-03-18 13:59:22 +0100

    Adjust the interface, add a separate ISource to ITimeSync

--- interfaces/ITimeSync.h
+++ interfaces/ITimeSync.h
@@ -26,13 +26,34 @@ namespace Thunder {
 namespace Exchange {
 
     // @json 1.0.0 @text:legacy_lowercase
-    // This interface gives direct access to a time synchronize / update
     struct EXTERNAL ITimeSync : virtual public Core::IUnknown {
+
+        // This interface gives direct access to a time synchronize / update
+        struct EXTERNAL ISource : virtual public Core::IUnknown {
+
+            enum { ID = ID_TIMESYNC_ISOURCE };
+
+            struct EXTERNAL INotification : virtual public Core::IUnknown {
+
+                enum { ID = ID_TIMESYNC_ISOURCE_NOTIFICATION };
+
+                // Some change happened with respect to the Network..
+                virtual void Completed() = 0;
+            };
+
+            virtual void Register(INotification* notification) = 0;
+            virtual void Unregister(INotification* notification) = 0;
+
+            virtual uint32_t Synchronize() = 0;
+            virtual void Cancel() = 0;
+            virtual string Source() const = 0;
+            virtual uint64_t SyncTime() const = 0;
+        };
 
         enum { ID = ID_TIMESYNC };
 
         struct TimeInfo {
-            string time /* @brief Synchronized time (in ISO8601 format); empty string if the time has never been synchronized (e.g. 2019-05-07T07:20:26Z) */;
+            Core::Time time /* @brief Synchronized time (in ISO8601 format); empty string if the time has never been synchronized (e.g. 2019-05-07T07:20:26Z) */;
             Core::OptionalType<string> source /* @brief The synchronization source like an NTP server (e.g. ntp://example.com) */;
         };
 
@@ -42,35 +63,34 @@ namespace Exchange {
             enum { ID = ID_TIMESYNC_NOTIFICATION };
 
             // @brief Signals a time change
-            // @description Some change happened with respect to the Network
             // @alt:deprecated timechange
-            virtual void Completed() = 0;
+            // @description Some change happened with respect to the Network
+            virtual void TimeChanged() = 0;
         };
 
         virtual Core::hresult Register(INotification* const notification) = 0;
         virtual Core::hresult Unregister(const INotification* const notification) = 0;
 
         // @brief Synchronizes time
+        // @deprecated
         // @description Use this method to synchronize the system time with the currently configured time source. If automatic time synchronization is initially disabled or canceled, it will be restarted
         // @retval ERROR_INPROGRESS: Returned when the method is called while previously triggered synchronization is in progress
         // @retval ERROR_INCOMPLETE_CONFIG: Returned when the source configuration is missing or invalid
-        virtual Core::hresult Synchronize() = 0;
-
-        // @brief Cancel the time synchronization process
-        virtual Core::hresult Cancel() = 0;
+        DEPRECATED virtual Core::hresult Synchronize() = 0;
 
         // @property
-        // @brief Get the synchronization source
-        // @param source: The synchronization source like an NTP server (e.g. ntp://example.com)
-        virtual Core::hresult Source(string& source /* @out */) const = 0;
-
-        // @property
-        virtual Core::hresult Time(Core::Time& time /* @out */) const = 0;
-
-        // @property
-        // @deprecated
-        // @brief Most recent synchronized time
+        // @brief Time of the most recent synchronization
+        // @retval ERROR_UNAVAILABLE: There has not been any successful time synchronization yet
         virtual Core::hresult SyncTime(TimeInfo& info /* @out */) const = 0;
+
+        // @property
+        // @brief Current system time
+        // @description Upon setting this property automatic time synchronization will be stopped. Usage of this property is deprecated and the SubSystem control plugin can be used as an alternative to achieve the same
+        // @retval ERROR_BADREQUEST: The time is invalid
+        virtual Core::hresult Time(Core::Time& time /* @out */) const = 0;
+        // @deprecated
+        DEPRECATED virtual Core::hresult Time(const Core::Time& time) = 0;
+
     };
 
 } // namespace Exchange
