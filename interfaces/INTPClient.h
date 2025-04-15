de5e0e5d15f301b8e3997e42dda801d07a7ead14
biju-vi <biju.vijayanindiradevi@sky.uk>
2025-04-15 10:35:46 +0000

    fixing review comments

--- interfaces/INTPClient.h
+++ interfaces/INTPClient.h
@@ -27,99 +27,29 @@ namespace Exchange {
     struct EXTERNAL INTPClient : virtual public Core::IUnknown {
         enum { ID = ID_NTPCLIENT };
         
-        enum NTPClientStatus: uint8_t {
-            NTPCLIENT_DISABLED = 0,
-            NTPCLIENT_UNSYNCHRONIZED,
-            NTPCLIENT_SYNCHRONIZED,
-            NTPCLIENT_ERROR
-        };
-
-        enum NTPClientTransmissionMode: uint8_t {
-            NTPCLIENT_MODE_UNICAST = 0,
-            NTPCLIENT_MODE_BROADCAST,
-            NTPCLIENT_MODE_MULTICAST,
-            NTPCLIENT_MODE_MANYCAST
-        };
-
-        enum NTPClientSyncMode: uint8_t {
-            NTPCLIENT_MODE_QUICK_SYNC = 0,
-            NTPCLIENT_MODE_NORMAL = 1,
+        enum IPMode : uint8_t {
+            Auto, // tries either ipv4 or ipv6 depending on router stack and endpoint availability
+            IPv4Only, // only attempts ntp over IPv4
+            IPv6Only // only attempts ntp over IPv6
         };
         
-        struct NTPClientConfiguration {
-            NTPClientTransmissionMode mode; // Unicast / Broadcast / Multicast / Manycast
-            uint32_t port; // the port used to send NTP packets
-            uint32_t version; 
-            //Comma-separated list of strings. Points to a CSV list of NTP servers or pools. 
-            //A NTP server can either be specified as an IP address or a host name. 
-            //It is expected that the NTP client resolves multiple addresses which can change over time when ResolveAddresses is enabled.
-            string servers;
-            // When this option is enabled the NTP client must resolve the multiple addresses 
-            //associated with the host name(s) that are specified in the Servers field.
-            bool resolveAddresses;
-            // When ResolveAddresses is enabled, This parameter specifies the maximum number of IP addresses 
-            // that the NTP client can resolve. 0 means that all addresses must be resolved.
-            uint32_t resolveMaxAddresses;
-            // Use symmetric active association mode. This device may provide synchronization to the configured NTP server.
-            bool peer;
-            //	This is the minimum polling interval, in seconds to the power of two, 
-            //  allowed by any peer of the Internet system, currently set to 6 (64 seconds).
-            uint32_t minPoll;
-            // This is the maximum polling interval, in seconds to the power of two, 
-            // allowed by any peer of the Internet system, currently set to 10 (1024 seconds)
-            uint32_t maxPoll;
-            // If the IBurst parameter is enabled, and this is the first packet sent when 
-            // the server has been unreachable, the client sends a burst. This is useful to 
-            // quickly reduce the synchronization distance below the distance threshold and synchronize the clock. 
-            bool iBurst;
-            // If the Burst parameter is enabled and the server is reachable and 
-            // a valid source of synchronization is available, the client sends a 
-            // burst of BCOUNT (8) packets at each poll interval. The interval between packets 
-            // in the burst is two seconds. This is useful to accurately measure jitter with long poll intervals. [RFC5905].
-            uint32_t burst;
-            // The IP Interface associated with the Client entry.
-            string interface;
-            // Specifies how the client sockets are bound.
-            // Address (The client sockets are bound to a local IP address)
-            // Device (The client sockets are bound to a network device. This can be useful when the local address is dynamic)
-            string bindType;
-        };
-
         // @event
         struct EXTERNAL INotification : virtual public Core::IUnknown {
             enum { ID = ID_NTPCLIENT_NOTIFICATION };
 
-            // Some change happened with respect to the Network..
-            virtual void Completed() = 0;
+            // Time has been synchronised
+            virtual void TimeSynchronised() = 0;
         };
 
         ~INTPClient() override = default;
         virtual uint32_t Register(INTPClient::INotification* sink) = 0;
         virtual uint32_t Unregister(INTPClient::INotification* sink) = 0;
 
-        // @property
         // @brief sysnchronise the time
         // @description sysnchronise the time by calling client utility(eg:chrony)
         // @param mode: synchronisation mode 
-        virtual uint32_t SynchroniseTime(const NTPClientSyncMode& mode /* @in */) = 0;
+        virtual uint32_t SynchroniseTime(const string& server /* @in */, const IPMode& mode /* @in */) = 0;
 
-        // @property
-        // @brief status of synchronization
-        // @description Get status whether time is synchronized or not
-        // @param status: Enum value  
-        virtual uint32_t GetStatus(NTPClientStatus &status /*@out*/ ) const = 0;
-
-        // @property
-        // @brief Current synchronized system time 
-        // @description Get Current synchronized system time in ISO8601 format (e.g. 2019-05-07T07:20:26Z)
-        // @param time: System time in ISO8601 format 
-        virtual uint32_t GetSyncTime(string& time /* @out */) const = 0;
-
-        // @property
-        // @brief Set configuration
-        // @description Set configuration in config file for the client utility(eg: chrony.conf)
-        // @param config: configuration structure 
-        virtual uint32_t SetConfiguration(const NTPClientConfiguration& config /* @in */) = 0;
     };
 
 } // namespace Exchange
