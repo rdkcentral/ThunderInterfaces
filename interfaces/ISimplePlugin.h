bab5b79e4247e666200cc76d7a2e6ee14301837d
msieben <4319079+msieben@users.noreply.github.com>
2025-08-26 08:16:30 +0000

    [interfaces/ISimplePlugin] : use more descriptive names for interface methods

--- interfaces/ISimplePlugin.h
+++ interfaces/ISimplePlugin.h
@@ -5,7 +5,7 @@
 namespace Thunder {
 namespace Exchange {
 
-// Generate JSON_RPC in addition to COM_RPC by adding this tag
+// Generate JSON_RPC in addition to COMRPC by adding this tag
 // @json 1.0.0
 struct EXTERNAL ISimplePlugin : virtual public Core::IUnknown {
     enum { ID = ID_SIMPLEPLUGIN };
@@ -14,28 +14,21 @@ struct EXTERNAL ISimplePlugin : virtual public Core::IUnknown {
     struct EXTERNAL INotification : virtual public Core::IUnknown {
         enum { ID = ID_SIMPLEPLUGIN_NOTIFICATION };
 
-        virtual void LifeChangingEvent(const string& description) = 0;
+        virtual void ProcessEvent(const string& description) = 0;
     };
 
-    // Register for COM_RPC notifications 
+    // Register for COMRPC notifications 
     virtual uint32_t Register(ISimplePlugin::INotification* sink /* @in */) = 0;
 
-    // Unregister for COM_RPC notifications 
+    // Unregister for COMRPC notifications 
     virtual uint32_t Unregister(ISimplePlugin::INotification* sink /* @in */) = 0;
 
     // Arbitrary method
-    virtual uint32_t DoSomething() = 0;
+    virtual uint32_t ExecuteTask() = 0;
 
-    // Start the implemented service
-    virtual uint32_t ServiceStart(VARIABLE_IS_NOT_USED uint32_t waitTime /* @in */) = 0;
+    virtual uint32_t InitializeTask(const uint32_t waitTime /* @in */) = 0;
 
-    // Stop the implemented service
-    virtual uint32_t ServiceStop(VARIABLE_IS_NOT_USED uint32_t waitTime /* @in */) = 0;
+    virtual uint32_t DeinitializeTask(const uint32_t waitTime /* @in */) = 0;
 };
 
 } } // namespace Thunder::Exchange
-
-
-// Abstract
-// Naming
-// const
