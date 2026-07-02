8deb94fa4c121d8db3f0437d7c5077224a1bb5d8
Sankalp Maneshwar <sankalpmaneshwar46@outlook.com>
2026-07-02 14:19:14 +0530

    Update QAIds.h

--- qa_interfaces/QAIds.h
+++ qa_interfaces/QAIds.h
@@ -75,12 +75,9 @@ namespace QualityAssurance {
         ID_TESTKEEP_NOTIFICATION                     = ID_TESTTEXTOPTIONS + 5,
         ID_TESTCUSTOM                                = ID_TESTTEXTOPTIONS + 6,
         ID_TESTCUSTOM_NOTIFICATION                   = ID_TESTTEXTOPTIONS + 7,
-<<<<<<< development/Benchmark_QA
-=======
 
         ID_TESTPLUGIN                                = RPC::IDS::ID_EXTERNAL_QA_INTERFACE_OFFSET + 0x060,
         ID_TESTPLUGIN_NOTIFICATION                   = ID_TESTPLUGIN + 1
->>>>>>> master
 
         ID_BENCHMARK                                 = RPC::IDS::ID_EXTERNAL_QA_INTERFACE_OFFSET + 0x040,
         ID_BENCHMARK_NOTIFICATION                    = ID_BENCHMARK + 1,
